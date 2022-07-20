e5ebbf999ce5d872319bc38411c4ed7a1a9ecafc
John Grohman <grohmanjp@gmail.com>
2022-07-20 00:33:03 -0500

    added about me section

--- my-app/src/Data/AboutData.js
+++ my-app/src/Data/AboutData.js
@@ -1,9 +1,12 @@
+import acm from '../assets/images/acm.png';
+import lca from '../assets/images/lca.png';
+import rov from '../assets/images/rover.png';
+
 export const AboutData = [
     {
         title: "ACM Web",
-        timeframe: "Aug 2021 - Present",
         role: "Front-End Developer",
-        image: "Image",
+        image: acm,
         bullets: [
             "Utilize React.js to create front-end features as a team of developers.",
             "Fixed front-end event date formatting issue on the ACM production site.",
@@ -12,9 +15,8 @@ export const AboutData = [
     },
     {
         title: "Lambda Chi Alpha",
-        timeframe: "Jan 2022 - Present",
         role: "Secretary",
-        image: "Image",
+        image: lca,
         bullets: [
             "Manage Outlook calendars, Outlook groups, and Slack",
             "Regularly update spreadsheets and statuses of members using Google Sheets and OmegaFi",
@@ -23,9 +25,8 @@ export const AboutData = [
     },
     {
         title: "Mars Rover Design Team",
-        timeframe: "Aug 2020 - Jan 2021",
         role: "Autonomous Programmer",
-        image: "Image",
+        image: rov,
         bullets: [
             "Developed search pattern and navigation algorithms as a member of the autonomy sub team.",
             "Wrote autonomous rover detection and navigation Python programs that allowed the rover to naviggate itself around a detected obstacle.",
--- my-app/src/pages/AboutMe.js
+++ my-app/src/pages/AboutMe.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import "../App.css";
+import AboutCard from '../Components/AboutCard';
+import { AboutData } from '../Data/AboutData.js';
 
-const AboutMe = () => (
+function AboutMe() {
+  return (
     <div className='AboutMe' id='aboutme'>
-        <h1>About-Me</h1>
-        <h2>Resume</h2>
+      <h1>About-Me</h1>
+      <p className='bio'>Welcome! My name is John and I'm currently studying computer scicne at Missouri S&T. Here's some more about my involvement on campus:</p>
+      { AboutData.map((val, key) => {
+        return (
+        <AboutCard
+        title = {val.title}
+        timeframe = {val.timeframe}
+        role = {val.role}
+        image = {val.image}
+        bullets = {val.bullets}
+        />
+
+        );
+        })}
+
+
+      <h2>Resume</h2>
     </div>
-)
+  )
+}
 
 export default  AboutMe;
