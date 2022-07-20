0410207f98f3c1a52220eb2e86fc04d0c46103aa
John Grohman <grohmanjp@gmail.com>
2022-07-19 20:03:46 -0500

    organized data files

---
+++ my-app/src/Data/SidebarData.js
@@ -0,0 +1,18 @@
+export const SidebarData = [
+    {
+        title: "Home",
+        link: "home"
+    },
+    {
+        title: "About-Me",
+        link: "aboutme"
+    },
+    {
+        title: "Contact-Me",
+        link: "contactme"
+    },
+    {
+        title: "More",
+        link: "more"
+    },
+]
