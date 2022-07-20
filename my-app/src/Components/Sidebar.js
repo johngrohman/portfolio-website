77019fe9b92ebf92caae56c5e8bdb01095b5322d
John Grohman <grohmanjp@gmail.com>
2022-07-19 20:05:30 -0500

    updated links

--- my-app/src/Components/Sidebar.js
+++ my-app/src/Components/Sidebar.js
@@ -4,7 +4,7 @@ import githubIcon from '../assets/images/github_logo_icon_white.png';
 import linkedinIcon from '../assets/images/linkedin_logo_icon_white.png';
 import codeIcon from '../assets/images/code_white.png';
 import "../App.css";
-import { SidebarData } from './SidebarData.js';
+import { SidebarData } from '../Data/SidebarData.js';
 
 function Sidebar() {
     return (
@@ -12,8 +12,8 @@ function Sidebar() {
             <img src={profileImage} alt="profile" id="profile-picture"/>
             <h2>John Grohman</h2>
             <div id="container">
-            <a href="https://www.linkedin.com/in/johngrohman/" target="_blank"><img src={linkedinIcon} alt="linkedin" /></a>
-            <a href="https://github.com/johngrohman" target="_blank"><img src={githubIcon} alt="github" /></a>
+            <a href="https://www.linkedin.com/in/johngrohman/" target="_blank" rel="noreferrer"><img src={linkedinIcon} alt="linkedin" /></a>
+            <a href="https://github.com/johngrohman" target="_blank" rel="noreferrer"><img src={githubIcon} alt="github" /></a>
          </div>
             <ul className="SidebarNav">
                 {SidebarData.map((val, key) => {
@@ -22,7 +22,7 @@ function Sidebar() {
                             key={key} 
                             className='SidebarNavRow'
                             onClick={()=> {
-                                window.location.pathname = val.link
+                                window.location.hash = val.link
                             }}
                         >
                         <div>{val.title}</div>
@@ -30,7 +30,7 @@ function Sidebar() {
                     );
                 })}
             </ul>
-            <img src={codeIcon} alt="code" id="code"/>
+            <a href="https://github.com/johngrohman/Portfolio-Website" target="_blank" rel="noreferrer"><img src={codeIcon} alt="code" id="code"/></a>
         </div>
     );
 }
