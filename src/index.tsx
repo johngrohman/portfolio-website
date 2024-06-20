5f1e836a685b1468e625919858402d2cee709869
John Grohman <grohmanjp@gmail.com>
2024-06-19 20:51:57 -0500

    Implemented react router in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SideBar from './Components/Navigation/Sidebar';
-import Main from './Components/Main';
 // import ErrorBoundary from './Components/ErrorBoundary';
+import App from './app';
 import './index.scss';
 
-const App = () => (
-    <div className='app'>
-        <SideBar />
-        <Main />
-    </div>
-);
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './error-page';
+
+const router = createBrowserRouter([
+    {
+        path: '',
+        element: <App />,
+        errorElement: <ErrorPage />,
+    },
+]);
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
     const root = ReactDOM.createRoot(rootElement);
     root.render(
-        <App />
+        <RouterProvider router={router} />
     );
 } else {
     console.log('Element with id root not found in the document');
