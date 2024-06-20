b0fea775baa70eaff67dee211ef14d9873f3ebcd
John Grohman <grohmanjp@gmail.com>
2024-06-19 20:50:06 -0500

    Updated contact page

--- src/Components/ContactCard/index.tsx
+++ src/Components/ContactCard/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './contactcard.scss';
 
 const ContactCard = () => (
     <div className='ContactCard'>
-        <img src={require('../../assets/images/profile.jpg')} alt='profilepic' />
-        <p>John Grohman</p>
-        <p>(314) 313-4703</p>
-        <p>grohmanjp@gmail.com</p>
+        <ul className='contactList'>
+            <li className='contactRow'><a href='mailto:grohmanjp@gmail.com'><i className="bi-envelope"></i> Email: grohmanjp@gmail.com</a></li>
+            <li className='contactRow'><a href='tel:+1 314-313-4703'><i className="bi-telephone"></i> Call: (314) 313-4703</a></li>
+            <li className='contactRow'><a href='sms:+1 314-313-4703'><i className="bi-chat"></i> Text: (314) 313-4703</a></li>
+            <li className='contactRow'><a href='https://www.linkedin.com/in/johngrohman/' target='_blank' rel='noreferrer'><i className="bi-linkedin"></i> Connect: Linkedin</a></li>
+        </ul>
     </div>
 );
 
--- src/Pages/Contact/index.tsx
+++ src/Pages/Contact/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ContactCard from '../../Components/ContactCard';
+import ContactCard from '../../components/ContactCard';
 import './contact.scss';
-import SectionTitle from '../../Components/SectionTitle';
+import SectionTitle from '../../components/SectionTitle';
 
 const ContactMe = () => (
     <div className='ContactMe' id='contactme'>
-        <SectionTitle title='Contact' color='#00FF29' />
-        <h1>Contact-Me</h1>
+        <SectionTitle title='Contact' color='#4CBB17' />
+        <h1>Get in touch!</h1>
         <ContactCard />
     </div>
 );
